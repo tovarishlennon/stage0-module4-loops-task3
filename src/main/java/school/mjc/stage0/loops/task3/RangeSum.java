ab04fc25606030b2ed2f52484e4c99a14e1e0964
Azim Shamuratov <a.shamuratov@atmos.uz>
2022-09-14 17:25:41 +0500

    Task #3. For Loops

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+        int acc = 0;
+        for (int i = firstBoarder; i <= secondBoarder; i++) {
+            acc += i;
+        }
+        System.out.println(acc);
     }
 }
